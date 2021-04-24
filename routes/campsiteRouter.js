beec1362914cb3ff01e082ad4bbe99ac9cd6df5f
Ramiro Melo <ramiromelo@gmail.com>
2021-04-24 13:13:55 -0700

    Workshop 3

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -15,7 +15,7 @@ campsiteRouter.route('/')
     })
     .catch(err => next(err));
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Campsite.create(req.body)
     .then(campsite => {
         console.log('Campsite Created ', campsite);
@@ -29,7 +29,7 @@ campsiteRouter.route('/')
     res.statusCode = 403;
     res.end('PUT operation not supported on /campsites');
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Campsite.deleteMany()
     .then(response => {
         res.statusCode = 200;
@@ -54,7 +54,7 @@ campsiteRouter.route('/:campsiteId')
     res.statusCode = 403;
     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Campsite.findByIdAndUpdate(req.params.campsiteId, {
         $set: req.body
     }, { new: true })
@@ -65,7 +65,7 @@ campsiteRouter.route('/:campsiteId')
     })
     .catch(err => next(err));
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Campsite.findByIdAndDelete(req.params.campsiteId)
     .then(response => {
         res.statusCode = 200;
@@ -117,7 +117,7 @@ campsiteRouter.route('/:campsiteId/comments')
     res.statusCode = 403;
     res.end(`PUT operation not supported on /campsites/${req.params.campsiteId}/comments`);
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite) {
@@ -166,31 +166,39 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`);
 })
 .put(authenticate.verifyUser, (req, res, next) => {
-    Campsite.findById(req.params.campsiteId)
+    //Campsite.findByIdAndUpdate(req.params.campsiteId)
+    Campsite.findByIdAndUpdate(req.params.campsiteId, {
+        $set: req.body
+    }, { new: true })
+
     .then(campsite => {
-        if (campsite && campsite.comments.id(req.params.commentId)) {
-            if (req.body.rating) {
-                campsite.comments.id(req.params.commentId).rating = req.body.rating;
-            }
-            if (req.body.text) {
-                campsite.comments.id(req.params.commentId).text = req.body.text;
-            }
-            campsite.save()
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsite);
-            })
-            .catch(err => next(err));
-        } else if (!campsite) {
-            err = new Error(`Campsite ${req.params.campsiteId} not found`);
-            err.status = 404;
-            return next(err);
-        } else {
-            err = new Error(`Comment ${req.params.commentId} not found`);
-            err.status = 404;
-            return next(err);
-        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+
+        // if (campsite && campsite.comments.id(req.params.commentId)) {
+        //     if (req.body.rating) {
+        //         campsite.comments.id(req.params.commentId).rating = req.body.rating;
+        //     }
+        //     if (req.body.text) {
+        //         campsite.comments.id(req.params.commentId).text = req.body.text;
+        //     }
+        //     campsite.save()
+        //     .then(campsite => {
+        //         res.statusCode = 200;
+        //         res.setHeader('Content-Type', 'application/json');
+        //         res.json(campsite);
+        //     })
+        //     .catch(err => next(err));
+        // } else if (!campsite) {
+        //     err = new Error(`Campsite ${req.params.campsiteId} not found`);
+        //     err.status = 404;
+        //     return next(err);
+        // } else {
+        //     err = new Error(`Comment ${req.params.commentId} not found`);
+        //     err.status = 404;
+        //     return next(err);
+        // }
     })
     .catch(err => next(err));
 })
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,17 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+// router.get('/', function(req, res, next) {
+//     res.send('respond with a resource');
+// });
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+    return User.find()
+        .then((users) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(users);
+        })
+        .catch((err) => next(err));
 });
 
 router.post('/signup', (req, res) => {
